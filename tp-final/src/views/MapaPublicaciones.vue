5e2c8b573b07fcace5b5c1320e25642d484ab79b
gaston <gaston.92@hotmail.com.ar>
2020-11-14 16:14:10 -0300

    modifico mapa publicaciones

--- tp-final/src/views/MapaPublicaciones.vue
+++ tp-final/src/views/MapaPublicaciones.vue
@@ -7,13 +7,45 @@
           <h3>Filtrar por:</h3>
           <hr />
           <b-list-group>
-            <b-list-group-item button variant="info">Buzón 1</b-list-group-item>
-            <b-list-group-item button variant="info">Buzón 2</b-list-group-item>
-            <b-list-group-item button variant="info">Buzón 3</b-list-group-item>
-            <b-list-group-item button variant="info">Buzón 4</b-list-group-item>
-            <b-list-group-item button variant="info">Buzón 5</b-list-group-item>
-            <b-list-group-item button variant="info">Buzón 6</b-list-group-item>
-            <b-list-group-item button variant="info">Buzón 7</b-list-group-item>
+            <b-list-group-item button variant="info" @click="activarItems"
+              >Buzón 1</b-list-group-item
+            >
+            <!-- <b-list-group-item
+              button
+              variant="info"
+              @click="activarItem(items[1])"
+              >Buzón 2</b-list-group-item
+            >
+            <b-list-group-item
+              button
+              variant="info"
+              @click="activarItem(items[2])"
+              >Buzón 3</b-list-group-item
+            >
+            <b-list-group-item
+              button
+              variant="info"
+              @click="activarItem(items[3])"
+              >Buzón 4</b-list-group-item
+            >
+            <b-list-group-item
+              button
+              variant="info"
+              @click="activarItem(items[4])"
+              >Buzón 5</b-list-group-item
+            >
+            <b-list-group-item
+              button
+              variant="info"
+              @click="activarItem(items[5])"
+              >Buzón 6</b-list-group-item
+            >
+            <b-list-group-item
+              button
+              variant="info"
+              @click="activarItem(items[6])" -->
+            <!-- >Buzón 7</b-list-group-item
+            > -->
           </b-list-group>
         </b-col>
         <b-col>
@@ -23,8 +55,13 @@
               :key="i"
               :position="item.position"
               :icon="item.icon"
-            >
+               @click="mostrarVentana">
             </gmapsMarker>
+
+
+
+            <gmaps-info-window :v-show="!mostrar" :options="options" >
+              </gmaps-info-window>
           </gmapsMap>
         </b-col>
       </b-row>
@@ -32,7 +69,7 @@
   </div>
 </template>
 <script>
-import { gmapsMap, gmapsMarker } from "x5-gmaps";
+import { gmapsMap, gmapsMarker, gmapsInfoWindow } from "x5-gmaps";
 
 const greenMarker = require("../assets/campfire.png");
 
@@ -40,7 +77,8 @@ export default {
   name: "MapaBuzon",
   components: {
     gmapsMap,
-    gmapsMarker
+    gmapsMarker,
+    gmapsInfoWindow,
   },
   data() {
     return {
@@ -52,45 +90,67 @@ export default {
         rotateControl: true,
         scaleControl: true,
         streetViewControl: false,
-        zoomControl: true
+        zoomControl: true,
+      },
+
+      options: {
+        position: { lat: -34.607979, lng: -58.435875 },
+        content: 'Buzon 3',
       },
       items: [
         {
           position: { lat: -34.606868, lng: -58.435875 },
-          icon: greenMarker
+          seMuestra: false,
+          icon: greenMarker,
         },
 
         {
           position: { lat: -34.593988, lng: -58.489948 },
-          icon: greenMarker
+          seMuestra: false,
+          icon: greenMarker,
         },
         {
           position: { lat: -34.571406, lng: -58.416048 },
-          icon: greenMarker
+          seMuestra: false,
+          icon: greenMarker,
         },
         {
           position: { lat: -34.583528, lng: -58.409573 },
-          icon: greenMarker
+          seMuestra: false,
+          icon: greenMarker,
         },
         {
           position: { lat: -34.60967, lng: -58.390068 },
-          icon: greenMarker
+          seMuestra: false,
+          icon: greenMarker,
         },
         {
           position: { lat: -34.551013, lng: -58.480165 },
-          icon: greenMarker
+          seMuestra: false,
+          icon: greenMarker,
         },
         {
           position: { lat: -34.614216, lng: -58.458122 },
-          icon: greenMarker
-        }
-      ]
+          seMuestra: false,
+          icon: greenMarker,
+        },
+      ],
+      mostrar: false
     };
   },
-  mounted() {
-    this.items.forEach(element => {
-      this.$GMaps().then(GMaps => (element.animation = GMaps.Animation.BOUNCE));
-    });
-  }
+  methods: {
+    mostrarVentana() {
+      this.mostrar = true
+    }
+  },
+  computed: {
+    // itemsActivados() {
+    //   return this.items.filter((e) => e.seMuestra === true);
+    // },
+
+    activarItems() {
+      return this.items.map((e) => e.seMuestra === false);
+    },
+  },
 };
 </script>
